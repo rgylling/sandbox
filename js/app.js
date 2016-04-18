c392075fbeb927abc0e9ece1869dc053b6b185ca
goodwid <goodwid@gmail.com>
2016-04-18 11:47:37 -0700

    More jquery

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@
 $('input').on('click', snagData);
 
 function snagData() {
-  $.getJSON('js/item.json', displayData);
+  $.getJSON('js/item.json', displayData, true);
 }
 
 function displayData(data) {
-  console.log(data);
+  data.forEach(function (thing){
+    console.log(thing.name);
+    console.log(thing.type);
+    console.log(thing.age);
+
+  })
 }
+
+$('input').click();
