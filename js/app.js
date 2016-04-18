3f3858db5225ee2f2e69abbb45ff0d60889cdf72
goodwid <goodwid@gmail.com>
2016-04-18 10:52:33 -0700

    Added a button to initiate HTTP request

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 var xhr = new XMLHttpRequest();
 
-
 xhr.onreadystatechange = function () {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var arr = JSON.parse(xhr.responseText);
     showstuff(arr);
   }
-}
-xhr.open ("GET","js/item.json", true);
-xhr.send();
+};
+
+$('input').on('click', function () {
+  xhr.open ('GET','js/item.json', true);
+  xhr.send();
+});
 
 function showstuff(arr) {
   arr.forEach(function (data) {
