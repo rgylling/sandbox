3bad68f494db0b5199c18a2e4e238b3b3dd3b48e
goodwid <goodwid@gmail.com>
2016-04-20 12:50:49 -0700

    Sandboxed

--- js/app.js
+++ js/app.js
@@ -13,28 +13,31 @@ function topWords (obj, count) {
   var winner;
   var rank = [];
   for (i=0;i<count;i++) {
-    winner = Object.keys(obj).reduce(function(a, b){ return obj[a] > obj[b] ? a : b });
+    winner = Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
     rank[i] = [winner,obj[winner]];
     delete obj[winner];
   }
+  console.log('rank = ',rank);
   return rank.reduce(function (prev, curr) {
       prev[curr[0]] = curr[1];
       return prev;
   }, {});
 }
 
+console.log(topWords(obj,3));
 
-//var values = Object.keys(obj).map(function(key) {return obj[key]});
-var keys = Object.keys(obj);
-$d = $('#display');
-var table = '<table>\n'
-table += "<tr><th>Word</th><th>Times Used</th><tr>";
-
-keys.map(function (el) {
-  table += '<tr><td>'+ el + '</td><td>' + obj[el] + '</td>\n';
-})
-table += '</table>\n';
-$d.append(table);
+// //var values = Object.keys(obj).map(function(key) {return obj[key]});
+// var keys = Object.keys(obj);
+// $d = $('#display');
+// var table = '';
+// table += '<table>\n'
+// table += "<tr><th>Word</th><th>Times Used</th><tr>";
+//
+// keys.map(function (el) {
+//   table += '<tr><td>'+ el + '</td><td>' + obj[el] + '</td>\n';
+// })
+// table += '</table>\n';
+// $d.append(table);
 
 
 //
